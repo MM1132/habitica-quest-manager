9531306dfba6089bc8cfb1bcfff6cb406e059a2c
Robert Reimann <reimannrobert707@gmail.com>
2024-04-03 19:34:21 +0300

    Rewrote the way how main page gets its data

--- src/_habitica-data/translation.js
+++ src/_habitica-data/translation.js
@@ -1,5 +1,13 @@
+const fs = require('fs');
+
+const questsContentPath = './locales/en/questsContent.json';
+const questsContentData = JSON.parse(fs.readFileSync(questsContentPath, 'utf8'));
+
+const contentPath = './locales/en/content.json';
+const contentData = JSON.parse(fs.readFileSync(contentPath, 'utf8'));
+
 function t(key) {
-  return key;
+  return questsContentData[key] || contentData[key];
 }
 
 module.exports = { t };
