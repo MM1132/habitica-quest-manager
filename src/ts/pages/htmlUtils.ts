bf6a3770da717f099a5584f17bf8dbe85a9b492a
Robert Reimann <reimannrobert707@gmail.com>
2024-04-12 09:25:48 +0300

    routing works, pages preloaded, starting/inviting quests by showing loading icon in the button, loading screen for every page that has to load data

--- src/ts/pages/htmlUtils.ts
+++ src/ts/pages/htmlUtils.ts
@@ -1,6 +1,17 @@
-export const include = (filename: string) => {
-  return HtmlService.createTemplateFromFile(filename)
+export const include = (filename: string, props?: any) => {
+  const template = HtmlService.createTemplateFromFile(filename);
+
+  template.props = props || {};
+
+  return template
     .evaluate()
     .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
     .getContent();
+};
+
+export const testFunction = (message: string) => {
+  if (message === 'error') {
+    throw new Error('Something bad happened');
+  }
+  return `test: ${message}`;
 };
